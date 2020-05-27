cf6c4fcf3a84494adc2701a1079532b71564815c
Mikastiv <m.leblanc_3@hotmail.com>
2020-05-26 22:20:43 -0400

    main reworked

--- RaytracerHW/Src/Main.cpp
+++ RaytracerHW/Src/Main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 
 #include "Parser.hpp"
+#include "Scene.hpp"
 
 int main()
 {
-    std::vector<Config> c{};
+    const auto ReadConfigFile = [](std::string_view file) {
+        std::vector<Config> c{};
 
-    try
-    {
-        c = Parser::ParseFile("scene1.test");
-    }
-    catch (const std::runtime_error& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    catch (...)
-    {
-        std::cerr << "Unhandled exception caught\n";
-    }
+        try
+        {
+            c = Parser::ParseFile(file);
+        }
+        catch (const std::runtime_error& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        catch (...)
+        {
+            std::cerr << "Unhandled exception caught\n";
+        }
+
+        return c;
+    };
+
+    Scene scene{ ReadConfigFile("scene1.test") };
 }
