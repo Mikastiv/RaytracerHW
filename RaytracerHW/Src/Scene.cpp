51ad969c5b3e8cb6e1ea6f83673d7fca94484b19
Mikastiv <m.leblanc_3@hotmail.com>
2020-06-12 17:56:46 -0300

    improved progress calculation

--- RaytracerHW/Src/Scene.cpp
+++ RaytracerHW/Src/Scene.cpp
@@ -21,6 +21,9 @@ auto Scene::Render() -> void
     const uint32_t progressBarSize = 40;
     uint32_t lastPercentage = 0;
 
+    const float pixelPercentageDelta = 1.0f / float(mImage.GetWidth() * mImage.GetHeight());
+    float percentage = pixelPercentageDelta;
+
     while (const auto pixel = mSampler.Sample())
     {
         const auto ray = mCamera.GenerateRay(*pixel);
@@ -31,17 +34,16 @@ auto Scene::Render() -> void
         mImage.PutPixel(x, y, c);
 
         const uint32_t currentPixel = y * mImage.GetWidth() + x;
-        const float imagePixelSize = float(mImage.GetWidth() * mImage.GetHeight());
-        const float percentage = currentPixel / imagePixelSize;
-        const uint32_t charCount = uint32_t(percentage * (progressBarSize + 1));
-        const uint32_t currentPercentage = uint32_t(percentage * 101);
+        const uint32_t charCount = uint32_t(percentage * progressBarSize);
+        const uint32_t currentPercentage = uint32_t(percentage * 100);
 
         if (currentPercentage > lastPercentage)
         {
-            std::cout << "\rProgress: " << std::setw(progressBarSize) << std::left << std::string(charCount, '=')
+            std::cout << "\rProgress: |" << std::setw(progressBarSize) << std::left << std::string(charCount, '=')
                       << "| " << currentPercentage << '%' << std::flush;
             lastPercentage = currentPercentage;
         }
+        percentage += pixelPercentageDelta;
     }
 
     mImage.Save(mOutputFile);
